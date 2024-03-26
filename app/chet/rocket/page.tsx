3ff7b78a54534211ae5c31944b05f5dca38e66af
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2024-03-26 09:59:09 -0400

    remove practice folders

--- app/chet/rocket/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function page() {
-    return <div>
-        <h1>The Rocket</h1>
-        <p>The best damn pitcher to ever play the game</p>
-    </div>
-
-}
--- app/dashboard/chet/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function page() {
-    return <div>
-        <h1>Chet Steadman's Landing Page</h1>
-        <p>The man, the myth, the legend</p>
-        </div>
-}
