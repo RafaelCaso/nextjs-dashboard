70b04308c5bd8a42e969dc1f511cf2499042816e
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2024-03-26 09:49:56 -0400

    add routes using file hierarchy

---
+++ app/chet/rocket/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return <div>
+        <h1>The Rocket</h1>
+        <p>The best damn pitcher to ever play the game</p>
+    </div>
+
+}
---
+++ app/dashboard/chet/page.tsx
@@ -0,0 +1,6 @@
+export default function page() {
+    return <div>
+        <h1>Chet Steadman's Landing Page</h1>
+        <p>The man, the myth, the legend</p>
+        </div>
+}
